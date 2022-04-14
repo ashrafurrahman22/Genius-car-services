0036c749903cb510cf44b82f4b81666298356c65
ashrafur22 <ashrafurrahman22@gmail.com>
2022-04-14 10:28:10 +0600

    last

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,59 +1,53 @@
-import React, { useRef } from 'react';
-import { Button, Form } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
+
 
 const Register = () => {
-    const nameRef = useRef('');
-    const emailRef  = useRef('');
-    const passwordRef = useRef('');
-    const navigate = useNavigate();
 
-    const handleRegister = event => {
-        event.preventDefault();
-        // const name = event.target.name.value;
-        // const email = event.target.email.value;
-        // const password = event.target.value;
-        // console.log(name)
-        const name = nameRef.current.value;
-        const email = emailRef.current.value;
-        const password = passwordRef.current.value;
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth)
 
-        console.log(name,email, password)
-    }
-    const navigateRegister = event => {
+
+      const navigate = useNavigate();
+      const navigateLogin = () => {
         navigate('/login')
-    }
+      }
+
+      if(user) {
+        navigate('/home')
+      }
+
+      const handleRegister = event => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        createUserWithEmailAndPassword(email, password)
+        console.log(name, email, password, user)
+      }
 
 
-    return (
-        <div className='mx-auto w-50 my-3 py-3'>
-            <h2>Please Sign Up</h2>
-            <Form onSubmit={handleRegister}>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Your Name</Form.Label>
-    <Form.Control ref={nameRef} required type="text" placeholder="Your Name" />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control ref={emailRef} required type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>password</Form.Label>
-    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
-  </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Register  </Button>
-</Form>
-        <p className='text-center py-3 my-3'>Already have an Account?  <button onClick={navigateRegister} className='text-white px-2 py-1 rounded-3 ms-2 bg-primary'>Log in</button> </p>
+  return (
+    <div className='register-form'>
+            <h2 style={{textAlign: 'center'}}>Please Register</h2>
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Your Name'/>
+                
+                <input type="email" name="email" id="" placeholder='Email Address' required/>
+                
+                <input type="password" name="password" id="" placeholder='Password' required/>
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}>Please Login</Link> </p>
         </div>
-    );
+  );
 };
 
 export default Register;
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getAuth} from 'firebase/auth'
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_apiKey,
-  authDomain:process.env.REACT_APP_authDomain,
-  projectId:process.env.REACT_APP_projectId,
-  storageBucket:process.env.REACT_APP_storageBucket,
-  messagingSenderId:process.env.REACT_APP_messagingSenderId,
-  appId:process.env.REACT_APP_appId,
+  apiKey: "AIzaSyCM-1QL1khNkXa-DH4JJ9hlq3FM6WpSoHg",
+  authDomain: "genius-car-service-743b3.firebaseapp.com",
+  projectId: "genius-car-service-743b3",
+  storageBucket: "genius-car-service-743b3.appspot.com",
+  messagingSenderId: "549881004497",
+  appId: "1:549881004497:web:dcb207ee1b7bed0c27a0f0"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
+
 export default auth;
